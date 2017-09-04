6713604a1843f07d4656d4f66cfc2d4c06679606
Austin Shalit <austinshalit@gmail.com>
2017-09-04 17:02:43 -0400

    Wrap everything in an afterEvaluate block

--- src/main/kotlin/edu/wpi/first/wpilib/javastyle/WPILibJavaStylePlugin.kt
+++ src/main/kotlin/edu/wpi/first/wpilib/javastyle/WPILibJavaStylePlugin.kt
@@ -14,39 +14,41 @@ class WPILibJavaStylePlugin : Plugin<Project> {
 
         // Only for java projects
         project.pluginManager.withPlugin("java") {
-            if (extension.checkstyle) {
-                project.plugins.apply(CheckstylePlugin::class.java)
-                project.extensions.configure(CheckstyleExtension::class.java, { checkstyle ->
-                    checkstyle.toolVersion = extension.checkstyleVersion.toString()
-                    checkstyle.config = project.resources.text.fromString(WPILibJavaStylePlugin::class.java.getResource("checkstyle.xml").readText())
-                })
-            }
+            project.afterEvaluate {
+                if (extension.checkstyle) {
+                    project.plugins.apply(CheckstylePlugin::class.java)
+                    project.extensions.configure(CheckstyleExtension::class.java, { checkstyle ->
+                        checkstyle.toolVersion = extension.checkstyleVersion.toString()
+                        checkstyle.config = project.resources.text.fromString(WPILibJavaStylePlugin::class.java.getResource("checkstyle.xml").readText())
+                    })
+                }
 
-            if (extension.pmd) {
-                project.plugins.apply(PmdPlugin::class.java)
-                project.extensions.configure(PmdExtension::class.java, { pmd ->
-                    pmd.isConsoleOutput = true
-                    pmd.reportsDir = project.buildDir.resolve("reports/pmd")
-                    pmd.ruleSetConfig = project.resources.text.fromString(WPILibJavaStylePlugin::class.java.getResource("pmd-ruleset.xml").readText())
-                })
-            }
+                if (extension.pmd) {
+                    project.plugins.apply(PmdPlugin::class.java)
+                    project.extensions.configure(PmdExtension::class.java, { pmd ->
+                        pmd.isConsoleOutput = true
+                        pmd.reportsDir = project.buildDir.resolve("reports/pmd")
+                        pmd.ruleSetConfig = project.resources.text.fromString(WPILibJavaStylePlugin::class.java.getResource("pmd-ruleset.xml").readText())
+                    })
+                }
 
-            if (extension.findbugs) {
-                project.plugins.apply(FindBugsPlugin::class.java)
-                project.extensions.configure(FindBugsExtension::class.java, { findBugs ->
-                    findBugs.effort = "max"
-                    findBugs.sourceSets = project.convention.getPlugin(JavaPluginConvention::class.java).sourceSets
-                })
+                if (extension.findbugs) {
+                    project.plugins.apply(FindBugsPlugin::class.java)
+                    project.extensions.configure(FindBugsExtension::class.java, { findBugs ->
+                        findBugs.effort = "max"
+                        findBugs.sourceSets = project.convention.getPlugin(JavaPluginConvention::class.java).sourceSets
+                    })
 
-                project.task("findbugsReport").doLast({
-                    val files = project.extensions.getByType(FindBugsExtension::class.java).reportsDir.listFiles()
-                    if (files.isNotEmpty()) {
-                        files.forEach {
-                            println(XmlParser().parse(it).get("BugInstance"))
+                    project.task("findbugsReport").doLast({
+                        val files = project.extensions.getByType(FindBugsExtension::class.java).reportsDir.listFiles()
+                        if (files.isNotEmpty()) {
+                            files.forEach {
+                                println(XmlParser().parse(it).get("BugInstance"))
+                            }
                         }
-                    }
-                })
-                project.tasks.withType(FindBugs::class.java).forEach({it.finalizedBy("findbugsReport")})
+                    })
+                    project.tasks.withType(FindBugs::class.java).forEach({ it.finalizedBy("findbugsReport") })
+                }
             }
         }
     }
